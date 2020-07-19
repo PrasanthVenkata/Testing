f08b41ca80d42f96cf113059977bb9ce427ae263
Prasanth Venkata <getprashanth@yahoo.com>
2020-07-19 14:25:53 +1000

    to fix sln not found error

---
+++ TestAutomation/TestingProject/FunctionalTestsSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace TestingProject
+{
+    [Binding]
+    public class FunctionalTestsSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            p0.Should().BeLessThan(10, "P0 should be less than 10");
+           
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            p0.Should().BeGreaterOrEqualTo(100, "P0 should be always 100");
+
+        }
+    }
+}
